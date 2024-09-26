cea18ba8ad27c581bc630c409a8c58b8efd03f16
Fernanda  Rocha Menon <fernandamenon93@gmail.com>
2024-09-26 19:26:06 -0300

    initial commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,14 @@
+import './Banner.css'
+
+function Banner() {
+    return(
+        <header className="banner">
+            <img src="/imagens/desing-thinking.png" alt="banner principal do meu protifólio"/>
+            <img src="/imagens/formacao-social-sustentabilidade.png" alt="banner principal do meu protifólio"/>
+            <img src="imagens/future-proof-organizations.png" alt="banner principal do meu protifólio"/>
+            <img src="imagens/inteligencia-artificial-computacional.png" alt="banner principal do meu protifólio"/>
+        </header>
+    )
+
+}
+export default Banner
